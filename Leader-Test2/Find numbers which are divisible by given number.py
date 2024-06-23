e90bc22235c20941ac91d1fa21e3c7cf913df1b4
ZukaAbashidze <zukaabashidze70@gmail.com>
2024-06-23 17:10:47 +0400

    Add files via upload

---
+++ Leader-Test2/Find numbers which are divisible by given number.py
@@ -0,0 +1,7 @@
+def divisible_by(numbers, divisor):
+    result = []
+    for num in numbers:
+        if num % divisor == 0:
+            result.append(num)
+    return result
+#8kyu
---
+++ Leader-Test2/get character from ASCII Value.py
@@ -0,0 +1,3 @@
+def get_char(c):
+    return chr(c)
+#8kyu
